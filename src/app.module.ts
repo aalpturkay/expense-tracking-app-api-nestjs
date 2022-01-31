8c61e4b4dc1bf7c368f9092febdd5600fcad4821
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-01-31 12:35:32 +0300

    delete started settings and add Auth, Expense and TypeOrm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ExpenseModule } from './expense/expense.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [AuthModule, ExpenseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    ExpenseModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'alpturkay',
+      database: 'expense-tracking',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
